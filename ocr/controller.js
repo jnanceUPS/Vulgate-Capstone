f67cc8a16f749ff180795b22e40ea0fa2e2a2368
Raymond Hermosillo <Raymond Hermosillo>
2016-04-15 13:34:52 -0700

    working on ocr page

--- ocr/controller.js
+++ ocr/controller.js
@@ -1,13 +1,26 @@
-var app = angular.module("tess", ['ngFileUpload']);
+var app = angular.module("tess", ['ng-upload']);
 
-app.controller('MainCtrl', ['$scope', 'Upload', '$http', function($scope, Upload, $http) {
+app.controller('MainCtrl', function() {
+    console.log("Hi there");
+    // $scope.onFileSelect = function($files){
+    //     console.log("on file select");
+    // 	for (var i = 0; i < $files.length; i++){
+    // 		var $file = $files[i];
+    //         console.log($file);
+    // 		Upload.upload ({
+    // 			url: 'my/upload/url',
+    // 			file: $file,
+    // 			progress: function(e){}
+    // 		}).then(function(data,status,headers,config){
+    // 			console.log(data);
+    // 		});
+    // 	}
+    // }   
 
-    $scope.tagline = 'To the moon and back!';   
+    // $scope.upload = function(file){
+    // Upload.upload({
+    //   url: 'upload/url',
+    //   data: {file: file}
+    // }).then(function( resp))
 
-    $scope.upload = function(file){
-    Upload.upload({
-      url: 'upload/url',
-      data: {file: file}
-    }).then(function( resp))
-
-}]);
+});
--- ocr/routes.js
+++ ocr/routes.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = function(app) {
 	 app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, './', './index.html')); // load index.html file
+            res.sendFile(path.join(__dirname, './', './page.html')); // load index.html file
         });
 
 }
