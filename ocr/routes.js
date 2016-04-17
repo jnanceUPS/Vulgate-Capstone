e71fd22bb0f959bcaa2b318f375b547fd1dab793
Raymond Hermosillo <Raymond Hermosillo>
2016-04-17 13:53:11 -0700

    added page.html and edited routes to correctly reflect the flow of using the website

--- ocr/routes.js
+++ ocr/routes.js
@@ -1,8 +1,32 @@
 var path = require('path');
+var formidable 	   = require('formidable'),
+	http = require('http'),
+	util = require('util'),
+	fs 	 = require('fs');
 
 module.exports = function(app) {
-	 app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, './', './page.html')); // load index.html file
+	 app.get('', function(req, res) {
+         res.sendFile(path.join(__dirname, './', './index.html')); // load index.html file
         });
+	 app.get('/view', function(req, res){
+	 	// fs.readFile('./temp.pdf', function(err, file) {
+   //    		res.writeHead(200, {"Content-Type" : "application/pdf" });
+   //    		res.write(file, "binary");
+   //    		res.end();
+   //  	});
+	 	res.sendFile(path.join(__dirname, './', './page.html'));
+	 })
+	 app.post('/upload', function(req, res){
+	 	var form = new formidable.IncomingForm();
+
+	 	form.parse(req, function(err, fields, files){
+	 		fs.rename(files.upload.path, './temp.pdf');
+	 		res.writeHead(200, {'content-type': 'text/html'});
+	 		res.write('Received upload: <a href="/view">View PDF</a>');
+	 		res.end();
+	 	});
+
+	 });
 
 }
+
--- ocr/server.js
+++ ocr/server.js
@@ -3,50 +3,17 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var bodyParser     = require('body-parser');
-var Busboy = require('busboy');
 
-// configuration ===========================================
-    
-// config files
-// var db = require('./config/db');
 
 // set our port
 var port = process.env.PORT || 8080; 
 
-// connect to our mongoDB database 
-// (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
-
-// get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json()); 
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-// app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
-
-app.post('/upload/url', function(req, res){
-	var busboy = new Busboy({ headers: req.headers });
-	busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-		});
-	busboy.on('finish', function() {
-		res.end();
-	});
-	req.pipe(busboy);
-});
+ app.use(express.static(__dirname + '/public')); 
 
 // routes ==================================================
- require('./routes')(app); // configure our routes
+  require('./routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
